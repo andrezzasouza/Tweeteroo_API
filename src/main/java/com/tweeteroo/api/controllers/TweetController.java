bf73cf44f80f54b474874bebdac4f8a86857a26d
Andrezza Souza <andrezzasouza@id.uff.br>
2023-04-10 05:50:30 -0300

    feat: get tweets by username

--- src/main/java/com/tweeteroo/api/controllers/TweetController.java
+++ src/main/java/com/tweeteroo/api/controllers/TweetController.java
@@ -1,6 +1,10 @@
 package com.tweeteroo.api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +21,11 @@ import jakarta.validation.Valid;
 public class TweetController {
   
   @Autowired TweetRepository repository;
+
+  @GetMapping("/{username}")
+  public List<Tweet> getTweetsByUser(@PathVariable String username) {
+    return repository.findByUsername(username);
+  }
 
   @PostMapping
   public String createTweet(@RequestBody @Valid TweetDTO req) {
--- src/main/java/com/tweeteroo/api/repositories/TweetRepository.java
+++ src/main/java/com/tweeteroo/api/repositories/TweetRepository.java
@@ -1,5 +1,7 @@
 package com.tweeteroo.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.tweeteroo.api.models.Tweet;
 
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+  List<Tweet> findByUsername(String username);
 }
